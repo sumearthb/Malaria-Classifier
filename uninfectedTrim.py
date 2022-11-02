22f0ea3c8e311fb8fd419f2ef0493424880f64ae
samarthbhat <samarthbhat@utexas.edu>
2022-11-02 18:29:24 -0500

    Added uninfected trimmer code

--- uninfectedTrim.py
+++ uninfectedTrim.py
@@ -1,1 +1,10 @@
-
+from PIL import Image
+import os
+count = 0
+directory = "C:\\Users\\sumea\\Downloads\\cell_images\\cell_images"
+os.chdir(directory + "\\resized_images\\Uninfected")
+for img in os.listdir(directory + "\\Uninfected"):
+    pil_img = Image.open(directory + "\\Uninfected\\" + img)
+    resized = pil_img.resize((100,100))
+    resized.save("img" + str(count) + ".png", "PNG")
+    count += 1
